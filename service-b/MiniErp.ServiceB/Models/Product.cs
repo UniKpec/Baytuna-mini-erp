613ce3193cdf3e9381e75676ec4920897bed18fb
Bedirhan <bedirhanyildirimm@gmail.com>
2026-08-19 20:11:18 +0300

    feat: add service B product CRUD

---
+++ service-b/MiniErp.ServiceB/Models/Product.cs
@@ -0,0 +1,9 @@
+namespace MiniErp.ServiceB.Models;
+
+public class Product
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public string SKU { get; set; } = string.Empty;
+    public decimal MarginPercent { get; set; }
+}
