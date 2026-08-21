356cbc0e83c7091c8f5d6e312f7671547606b2b3
Bedirhan Yıldırım <bedirhanyildirimm@gmail.com>
2026-08-21 23:28:07 +0300

    Add EF Core PostgreSQL integration

--- service-b/MiniErp.ServiceB/Program.cs
+++ service-b/MiniErp.ServiceB/Program.cs
@@ -1,4 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+using MiniErp.ServiceB;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseNpgsql(
+        builder.Configuration.GetConnectionString("DefaultConnection")
+    )
+);
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
